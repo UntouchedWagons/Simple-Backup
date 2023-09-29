b819ddda95c172c85bc8100963ecfe2e0d7f8f9b
UntouchedWagons <UntouchedWagons@fastmail.com>
2023-09-29 23:08:35 +0000

    Slight rewrite of run.sh should resolve #1

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,15 @@
 touch /var/log/cron.log
 env | egrep '^BACKUP' | cat - /tmp/backup-cronjob > /etc/cron.d/backup-cronjob
 crontab /etc/cron.d/backup-cronjob
-cron -f
+
+_term() { 
+  echo "Caught SIGTERM signal!" 
+  kill -TERM "$child" 2>/dev/null
+}
+
+trap _term SIGTERM
+
+cron -f &
+
+child=$! 
+wait "$child"
